6263bf2ae06cfc578c807a581f31cbab2df4c84e
Daniel Savage <dansvg@gmail.com>
2020-04-12 22:34:37 -0700

    Camera turning and movement

--- stage00.c
+++ stage00.c
@@ -4,13 +4,19 @@
 #include "graphic.h"
 #include <gu.h>
 
+static float player_x; /* The display position-X */
+static float player_y; /* The display position-Y */
+
+static float camera_rotation;
+
 static float camera_x; /* The display position-X */
 static float camera_y; /* The display position-Y */
 
 #define BULLET_COUNT 16
 
 #define CAMERA_MOVE_SPEED 0.01726f
-#define CAMERA_DISTANCE -14.f
+#define CAMERA_TURN_SPEED 0.03826f
+#define CAMERA_DISTANCE 51.f
 
 typedef struct {
   float x;
@@ -69,10 +75,14 @@ static Vtx bullet_test_geom[] =  {
 void initStage00(void)
 {
   int i;
-  int j;
 
-  camera_x = 0.0;
-  camera_y = 0.0;
+  player_x = 0.0f;
+  player_y = 0.0f;
+
+  camera_x = 0.0f;
+  camera_y = 0.0f;
+
+  camera_rotation = 0.f;
 
   for (i = 0; i < BULLET_COUNT; i++) {
     float r = i / (float)BULLET_COUNT * M_PI * 2.f;
@@ -88,12 +98,30 @@ void initStage00(void)
 
 }
 
+void addCubeToDisplayList()
+{
+  gSPVertex(glistp++,&(bullet_test_geom[0]), 4, 0);
+  gSP2Triangles(glistp++,0,1,2,0,0,2,3,0);
+  gSPVertex(glistp++,&(bullet_test_geom[4]), 4, 0);
+  gSP2Triangles(glistp++,0,1,2,0,0,2,3,0);
+  gSPVertex(glistp++,&(bullet_test_geom[8]), 4, 0);
+  gSP2Triangles(glistp++,0,1,2,0,0,2,3,0);
+  gSPVertex(glistp++,&(bullet_test_geom[12]), 4, 0);
+  gSP2Triangles(glistp++,0,1,2,0,0,2,3,0);
+  gSPVertex(glistp++,&(bullet_test_geom[16]), 4, 0);
+  gSP2Triangles(glistp++,0,1,2,0,0,2,3,0);
+  gSPVertex(glistp++,&(bullet_test_geom[20]), 4, 0);
+  gSP2Triangles(glistp++,0,1,2,0,0,2,3,0);
+}
+
 /* Make the display list and activate the task */
 void makeDL00(void)
 {
   Dynamic* dynamicp;
   char conbuf[20]; 
   int i;
+  Mtx playerTranslation;
+  Mtx playerRotation;
 
   /* Perspective normal value; I don't know what this does yet. */
   u16 perspNorm;
@@ -109,8 +137,8 @@ void makeDL00(void)
   gfxClearCfb();
 
   /* projection,modeling matrix set */
-  guPerspective(&dynamicp->projection, &perspNorm, 60.0f, (float)SCREEN_WD/(float)SCREEN_HT, 10.0f, 100.0f, 1.0f);
-  guLookAt(&dynamicp->viewing, camera_x, camera_y + CAMERA_DISTANCE, 50.f, camera_x, camera_y, 0.0f, 0.0f, 1.0f, 0.0f);
+  guPerspective(&dynamicp->projection, &perspNorm, 45.0f, (float)SCREEN_WD/(float)SCREEN_HT, 10.0f, 100.0f, 1.0f);
+  guLookAt(&dynamicp->viewing, camera_x + (cosf(camera_rotation - (M_PI * 0.5f) ) * CAMERA_DISTANCE), camera_y + (sinf(camera_rotation - (M_PI * 0.5f) ) * CAMERA_DISTANCE), 50.f, camera_x, camera_y, 0.0f, 0.0f, 0.0f, 1.0f);
 
   gSPPerspNormalize(glistp++, perspNorm);
 
@@ -120,25 +148,26 @@ void makeDL00(void)
   gDPPipeSync(glistp++);
   gDPSetCycleType(glistp++, G_CYC_1CYCLE);
   gDPSetRenderMode(glistp++, G_RM_ZB_OPA_SURF, G_RM_ZB_OPA_SURF2);
-  gSPClearGeometryMode(glistp++,0xFFFFFFFF);
-  gSPSetGeometryMode(glistp++,G_ZBUFFER | G_CULL_BACK | G_SHADE);
+  gSPClearGeometryMode(glistp++, 0xFFFFFFFF);
+  gSPSetGeometryMode(glistp++, G_ZBUFFER | G_CULL_BACK | G_SHADE);
 
+  // Render Player
+  guTranslate(&(playerTranslation), player_x, player_y, 0.f);
+  gSPMatrix(glistp++, OS_K0_TO_PHYSICAL(&(playerTranslation)), G_MTX_PUSH | G_MTX_MODELVIEW);
+
+  addCubeToDisplayList();
+
+  gDPPipeSync(glistp++);
+
+  gSPPopMatrix(glistp++, G_MTX_MODELVIEW);
+  gSPPopMatrix(glistp++, G_MTX_MODELVIEW);
+
+  // Render Bullets
   for (i = 0; i < BULLET_COUNT; i++) {
     guTranslate(&(BulletMatricies[i]), BulletPositions[i].x, BulletPositions[i].y, 0.f);
     gSPMatrix(glistp++, OS_K0_TO_PHYSICAL(&(BulletMatricies[i])), G_MTX_PUSH | G_MTX_MODELVIEW);
 
-    gSPVertex(glistp++,&(bullet_test_geom[0]), 4, 0);
-    gSP2Triangles(glistp++,0,1,2,0,0,2,3,0);
-    gSPVertex(glistp++,&(bullet_test_geom[4]), 4, 0);
-    gSP2Triangles(glistp++,0,1,2,0,0,2,3,0);
-    gSPVertex(glistp++,&(bullet_test_geom[8]), 4, 0);
-    gSP2Triangles(glistp++,0,1,2,0,0,2,3,0);
-    gSPVertex(glistp++,&(bullet_test_geom[12]), 4, 0);
-    gSP2Triangles(glistp++,0,1,2,0,0,2,3,0);
-    gSPVertex(glistp++,&(bullet_test_geom[16]), 4, 0);
-    gSP2Triangles(glistp++,0,1,2,0,0,2,3,0);
-    gSPVertex(glistp++,&(bullet_test_geom[20]), 4, 0);
-    gSP2Triangles(glistp++,0,1,2,0,0,2,3,0);
+    addCubeToDisplayList();
 
     gDPPipeSync(glistp++);
 
@@ -157,21 +186,35 @@ void makeDL00(void)
 		 NU_GFX_UCODE_F3DEX , NU_SC_NOSWAPBUFFER);
 
   if(contPattern & 0x1)
-    {
-      /* Change character representation positions */
-      nuDebConTextPos(0,12,23);
-      sprintf(conbuf,"analog X=%5.1f",camera_x);
-      nuDebConCPuts(0, conbuf);
+  {
 
-      nuDebConTextPos(0,12,24);
-      sprintf(conbuf,"analog Y=%5.1f",camera_y);
-      nuDebConCPuts(0, conbuf);
-    }
+    nuDebConTextPos(0,1,23);
+    sprintf(conbuf,"plrX=%5.1f", player_x);
+    nuDebConCPuts(0, conbuf);
+
+    nuDebConTextPos(0,1,24);
+    sprintf(conbuf,"plrY=%5.1f", player_y);
+    nuDebConCPuts(0, conbuf);
+
+    nuDebConTextPos(0,1,25);
+    sprintf(conbuf,"camR=%5.1f", camera_rotation);
+    nuDebConCPuts(0, conbuf);
+
+
+
+    nuDebConTextPos(0,1,26);
+    sprintf(conbuf,"camX=%5.1f", camera_x);
+    nuDebConCPuts(0, conbuf);
+
+    nuDebConTextPos(0,1,27);
+    sprintf(conbuf,"camY=%5.1f", camera_y);
+    nuDebConCPuts(0, conbuf);
+  }
   else
-    {
-      nuDebConTextPos(0,9,24);
-      nuDebConCPuts(0, "Controller1 not connect");
-    }
+  {
+    nuDebConTextPos(0,9,24);
+    nuDebConCPuts(0, "Controller 1 not connected; please restart");
+  }
     
   /* Display characters on the frame buffer */
   nuDebConDisp(NU_SC_SWAPBUFFER);
@@ -180,25 +223,59 @@ void makeDL00(void)
   gfx_gtask_no ^= 1;
 }
 
+float lerp(float v0, float v1, float t) {
+  return (1 - t) * v0 + t * v1;
+}
 
 /* The game progressing process for stage 0 */
 void updateGame00(void)
 {  
   int i;
+  float cosCamRot;
+  float sinCamRot;
+  float deltaX;
+  float deltaY;
+  float rotatedX;
+  float rotatedY;
 
   /* Data reading of controller 1 */
   nuContDataGetEx(contdata,0);
 
-  /* Change the display position by stick data */
-  camera_x += contdata->stick_x * CAMERA_MOVE_SPEED;
-  camera_y += contdata->stick_y * CAMERA_MOVE_SPEED;
-
   /* The reverse rotation by the A button */
-  if(contdata[0].trigger & A_BUTTON)
+  if(contdata[0].button & L_TRIG)
   {
-      //
+      camera_rotation += CAMERA_TURN_SPEED;
+
+      if (camera_rotation > (M_PI * 2.0f))
+      {
+        camera_rotation -= M_PI * 2.0f;
+      } 
   }
 
+  if(contdata[0].button & R_TRIG)
+  {
+      camera_rotation -= CAMERA_TURN_SPEED;
+
+      if (camera_rotation < 0.f)
+      {
+        camera_rotation += M_PI * 2.0f;
+      }
+  }
+
+  // Raw stick data (this should be adjusted for deadzones or emulator players on d-pads)
+  cosCamRot = cosf(-camera_rotation);
+  sinCamRot = sinf(-camera_rotation);
+  deltaX = contdata->stick_x * 0.03f;
+  deltaY = contdata->stick_y * 0.03f;
+  rotatedX = (deltaX * cosCamRot) + (deltaY * sinCamRot);
+  rotatedY = (-deltaX * sinCamRot) + (deltaY * cosCamRot);
+
+  player_x += rotatedX;
+  player_y += rotatedY;
+
+  // Lerp the camera
+  camera_x = lerp(camera_x, player_x, 0.13f);
+  camera_y = lerp(camera_y, player_y, 0.13f);
   
   for (i = 0; i < BULLET_COUNT; i++) {
     BulletPositions[i].x += BulletVelocities[i].x;
